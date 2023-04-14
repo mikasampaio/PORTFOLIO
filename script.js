6760c145b584999d99b88e411a29007f5ba5a6a0
mikasampaio <mikaasampaio6@gmail.com>
2023-04-14 20:22:18 -0300

    botão voltar ao topo

---
+++ script.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById("btnTop")
+
+btn.addEventListener("click", () => {
+    window.scrollTo(0,0)
+})
+
+document.addEventListener("scroll", ocult)
+
+const ocult = () => {
+    if (window.scrollY > 10) {
+        btn.style.display = "flex"
+    } else {
+        btn.style.display = "none"
+    }
+}
+
+ocult()
